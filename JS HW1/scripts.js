9f6d8f390d5725c35be85602f2d152129e2ceb11
Alex <fnt111333@gmail.com>
2023-10-16 15:51:21 +0300

    homework 1,2

---
+++ JS HW1/scripts.js
@@ -0,0 +1,114 @@
+let str = "Привет";
+let sum = 123;
+let num = 15.8;
+let flag = true;
+let txt = "true";
+
+console.log (str, typeof str);
+console.log (sum, typeof sum);
+console.log (num, typeof num);
+console.log (flag, typeof flag);
+console.log (txt, typeof txt);
+
+let a1= 5%3;
+console.log (a1, typeof a1);
+
+let a2= 3%5;
+console.log (a2, typeof a2);
+
+let a3= 5+"3";
+console.log (a3, typeof a3);
+
+let a4= '5'-3;
+console.log (a4, typeof a4);
+
+let a5= 75+'кг';
+console.log (a5, typeof a5);
+
+let a6= 785*653;
+console.log (a6, typeof a6);
+
+let a7= 100/25;
+console.log (a7, typeof a7);
+
+let a8= 0*0;
+console.log (a8, typeof a8);
+
+let a9= 0/2;
+console.log (a9, typeof a9);
+
+let a10= 89/0;
+console.log (a10, typeof a10);
+
+let a11= 98+2;
+console.log (a11, typeof a11);
+
+let a12= 5-98;
+console.log (a12, typeof a12);
+
+let a13= (8 + 56 * 4) / 5;
+console.log (a13, typeof a13);
+
+let a14= (9 - 12) * 7 / (5 + 2);
+console.log (a14, typeof a14);
+
+let a15= +"123";
+console.log (a15, typeof a15);
+
+let a16= 1 || 0;
+console.log (a16, typeof a16);
+
+let a17 = false || true;
+console.log (a17, typeof a17);
+
+let a18= true > 0.
+console.log (a18, typeof a18);
+
+let width= 10;
+let hight= 23;
+
+console.log(SPryam=width*hight + " см. кв.");
+
+let Pi=3.14159;
+let HCilindra = 10;
+console.log( VCilindra = HCilindra * Pi*(a7/2)**2+" м.кв."); 
+
+let r=5;
+console.log(SKruga=Pi*r*r+ 'см. кв.');
+
+let a=5, b=7, h=10, STrap;
+console.log(STrap=((a+b)/2)*h + " см. кв.");
+
+let s=2, p=10, years=5;
+console.log(Pereplata=(s*p/100)*years + ' млн. руб.');
+
+a=8;
+b=3;
+console.log(x1=(16-a-2*b)/2);
+console.log(x2=(a-15*b)/(b-6));
+console.log(x3=(23780)/(3+a+b));
+
+
+
+console.log('-------------------------');
+
+console.log(`Бывало, спит у ног собака,
+костер занявшийся гудит,
+и женщина из полумрака
+глазами зыбкими глядит.
+ 
+Потом под пихтою приляжет
+на куртку рыжую мою
+и мне, задумчивая, скажет:
+ 
+"А ну-ка, спой!..- и я пою."`)
+
+let w1= "индо земля зашаталась под ногами-и вырос,";
+let w2= "и заревел он голосом диким...";
+let w3= "блеснула молния и ударил гром,";
+let w4= "а так какое-то чудище, страшное и мохнатое,";
+let w5= "как будто из-под земли, перед купцом:";
+let w6= "Он подошёл и сорвал аленький цветочек.";
+let w7= "зверь не зверь, человек не человек,";
+let w8= "В ту же минуту, безо всяких туч,";
+console.log (w6 + " " + w8 + " " + w3 + " " + w1 + " " + w5 + " " + w7 + " " + w4 + " " + w2);
---
+++ JS HW2/scripts.js
@@ -0,0 +1,221 @@
+// Задание 1-2.
+
+// let name=prompt("Как тебя зовут?", "Иван");
+// let age=prompt("Сколько тебе лет?", "18");
+// let city=prompt("В каком городе ты живешь?", "Брест");
+// let phone=prompt("Введи свой телефонный номер", "+37529-856-52-52");
+// let email=prompt("Введи свою электронную почту", "tishko@gmail.com");
+// let company=prompt("Как называется твоя компания", "ТехноТранс");
+
+// document.write("Меня зовут " + name + ". Мне " + age + " лет. Я проживаю в городе " + city + " и работаю в компании " + company + ". Мои контактные данные:" + phone + ", " + email + ".");
+
+// document.write( '</br>' );
+
+// let date=2023-age;
+// document.write(name +" родился в " + date +" году.");
+
+
+//Задание 3.
+//    let code=prompt("Ввведите шестизначное число", "123456");
+
+
+//  if ( code[0] + code[1] + code[2] == code[3] + code[4] + code[5] ) {
+//      console.log("Да.");
+//  }
+//  else{
+//      console.log("Нет.");
+//  }
+
+
+
+
+
+// Задание 4.
+
+//  let a=0;
+// a>0 ? console.log('Верно')  : console.log(`Не верно.`);
+
+
+
+// Задание 5.
+
+//let a=10, b=2;
+// console.log(sum=a+b);
+// console.log(min=a-b);
+// console.log(inc=a*b);
+// console.log(div=a/b);
+
+// if (sum>1) {
+//     console.log(sum ** 2);   
+// }
+
+// Задание 6.
+// if (a>2 && a<11 && 6<=b && b<14 ) {
+//     console.log("Верно.");
+// }
+// else{
+//     console.log("Неверно.");
+// }
+
+//Задание 7.
+
+//let n=prompt("Введи число от 0 до 59", "");
+
+
+// if (n>=0 && n<=15 ) {
+//     alert("Первая четверть часа.");
+// }
+// else if (n>15 && n<=30 ) {
+//    alert("Вторая четверть часа.");
+// }
+// else if (n>30 && n<=45) {
+//     alert("Третья четверть часа.");
+// }
+// else if (n>45 && n<=59) {
+//        alert("Четвертая четверть часа.");
+// }
+// else {
+//     alert("Неверное число.");
+// }
+
+
+
+//Задание 8.
+
+// let day=prompt("Введи число от 1 до 31", "");
+
+
+// if (day>=1 && day<=10 ) {
+//     alert("Первая декада месяца.");
+// }
+// else if (day>=11 && day<=20 ) {
+//    alert("Вторая декада месяца.");
+// }
+// else if (day>=21 && day<=31) {
+//     alert("Третья декада месяца.");
+// }
+// else {
+//     alert("Неверное число.");
+// }
+
+
+//Задание 9.
+
+// let day=prompt("Введи любое количество дней.", "");
+
+// if (day>=365){
+//     years = day/365;
+// }
+// else {
+//     years = "меньше года."
+// }
+
+
+// if (day>=31){
+//     months = day/31;
+// }
+// else {
+//     months = "меньше месяца."
+// }
+
+
+// if (day>=7){
+//     weeks = day/7;
+// }
+// else {
+//     weeks = "меньше недели."
+// }
+
+// let hours = day*24;
+// let minuts = day * 1440;
+// let sec = day * 86400;
+
+
+// alert("Количество лет: " + years + " Количество месяцев: " + months + " Количество недель: " + weeks + " Количество часов: " + hours +  " Количество минут: " + minuts + " Количество секунд: " + sec );
+
+
+
+//Задание 10.
+
+
+// let day =prompt("Введи количество дней от 1 до 365.", "");
+
+// let month;
+
+// if (day>=1 && day<=31 ) {
+//         month = "Январь.";
+//     }
+//     else if (day>=32 && day<=59 ) {
+//        month = "Февраль.";
+//     }
+//     else if (day>=60 && day<=90) {
+//         month = "Март.";
+//     }
+//      else if (day>=91 && day<=120 ) {
+//         month = "Апрель.";
+//      }
+//      else if (day>=121 && day<=151) {
+//         month = "Май.";
+//     }
+//     else if (day>=152 && day<=181) {
+//         month = "Июнь.";
+//      }
+//      else if (day>=182 && day<=212) {
+//          month = "Июль.";
+//      }
+//      else if (day>=213 && day<=243 ) {
+//         month = "Август.";
+//      }
+//      else if (day>=244 && day<=273) {
+//          month = "Сентябрь.";
+//      }
+//      else if (day>=274 && day<=304 ) {
+//         month = "Октябрь.";
+//      }
+//      else if (day>=305 && day<=335 ) {
+//         month = "Ноябрь.";
+//      }
+//      else if (day>=336 && day<=365 ) {
+//         month = "Декабрь.";
+//      }
+
+//     else {
+//         month = ("Слишком большое число.");
+
+//     }
+
+
+
+
+// let partOfYear;
+// switch (month) {
+//     case "Январь." :
+//     case "Февраль." :
+//     case "Декабрь." :
+//         partOfYear = "Зима.";
+//         break;
+
+//     case "Март.":
+//     case "Апрель.":
+//     case "Май.":
+//         partOfYear = "Весна.";
+//         break;
+
+//     case "Июнь.":
+//     case "Июль.":
+//     case "Август.":
+//         partOfYear = "Лето.";
+//         break;
+
+//     case "Сентябрь.":
+//     case "Октябрь.":
+//     case "Ноябрь.":
+//         partOfYear = "Осень.";
+//         break;
+
+//     case "Слишком большое число.":
+//         partOfYear = "Больше года.";
+//         break;
+// }
+
+// alert("Месяц " + month + " Время года: " + partOfYear);
