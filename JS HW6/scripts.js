578625a5d8ba02324dfd353a531ac0f0f8a47f7a
Alex <fnt111333@gmail.com>
2023-11-06 15:30:58 +0300

    Add HW 7

--- JS HW6/scripts.js
+++ JS HW6/scripts.js
@@ -88,12 +88,25 @@
 // перед днями и месяцами, которые состоят из одной цифры (из 1.9.2014
 // сделает 01.09.2014).
 
-// let date= new Date();         //НЕ РЕШЕНО!!!
+// let date= new Date();     
 
-// date.setFullYear(2014, 2, 9)
+// function addNought(value) {
+//     if (value<10) return '0'+value;
+//     return value;
+// }
 
-// date.toLocaleString('ru')
-// console.log(date);
+// let year= date.getFullYear();
+// let month= date.getMonth();
+// let day= date.getDay();
+// let hour= date.getHours();
+// let minite= date.getMinutes();
+// let sec= date.getSeconds();
+
+
+
+// let myDate = `${hour}:${addNought(minite)}:${addNought(sec)} ${addNought(day)}.${addNought(month+1)}.${year}`
+
+// console.log(myDate);
 
 
 
@@ -103,6 +116,78 @@
 // найдет строки aba, abba, abbba по шаблону: буква 'a', буква 'b' любое
 // количество раз, буква 'a'.
 
-let str='aa aba abba abbba abca abea';
+// let str='aa aba abba abbba abca abea';
 
-console.log(str.match(/a/gi));
+// console.log(str.match(/\ab+a/gm).join());
+
+
+
+// Задача 8.
+// Напишите ф-цию строгой проверки ввода номер телефона в
+// международном формате (<код страны> <код города или сети> <номер
+// телефона>). Функция должна возвращать true или false. Используйте
+// регулярные выражения.
+
+
+// function phoneCheck(number) {
+//     let regul = /^\+?375 ?\(?(17|29|25|33|44)\)? ?-?(\d[ -]?)+\d$/gm;
+
+//    return regul.test(number);
+// }
+
+// console.log(phoneCheck(+375295226565));
+// console.log(phoneCheck('+375 (29) 8-222222'));
+// console.log(phoneCheck('8029 714-89-89'));
+// console.log(phoneCheck('375 29 8569699'));
+
+
+
+// Задача 9.
+// Напишите ф-цию строгой проверки адреса эл. почты с учетом следующих
+// условия:
+// - весь адрес не должен содержать русские буквы и спецсимволы, кроме
+// одной «собачки», знака подчеркивания, дефиса и точки;
+// - имя эл. почты (до знака @) должно быть длиной более 2 символов, причем
+// имя может содержать только буквы, цифры, но не быть первыми и
+// единственными в имени;
+// - после последней точки и после @, домен верхнего уровня (ru, by, com и т.п.)
+// не может быть длиной менее 2 и более 11 символов.
+// Функция должна возвращать true или false. Используйте регулярные
+// выражения.
+
+
+
+// function emailCheck(number) {
+//         let regul = /^[a-z0-9](\w+[\-\.\_]?)+\w+@((\w+[\-\.]?)+){2,11}\.[a-z]{2,11}$/gm;
+    
+//        return regul.test(number);
+//     }
+    
+//     console.log(emailCheck('kuku1999@mail.ru'));
+//     console.log(emailCheck('jhr@g.com'));
+
+
+
+
+// Задача 10.
+// Напишите ф-цию, которая из полного адреса с параметрами и без,
+// например: https://tech.onliner.by/2018/04/26/smart-do-200/?
+// utm_source=main_tile&utm_medium=smartdo200#zag3 , получит адрес
+// доменного имени (https://tech.onliner.by), остальную часть адреса без
+// параметров (/2018/04/26/smart-do-200/), параметры
+// (utm_source=main_tile&utm_medium=smartdo200) и хеш (#zag3). В адресе
+// может и не быть каких-либо составляющих. Ф-ция должна возвращать
+// массив.
+
+// НЕДОРЕШЕНО!!!
+
+// function emailCheck(number) {
+
+// let str = number;
+// let ansver = str.match(/(https?:\/\/[a-z0-9\.]+)(\/?.+\/)(.+)(#\w+)/gm);
+
+// return console.log(ansver);
+    
+// }
+    
+//     console.log(emailCheck('https://tech.onliner.by/2018/04/26/smart-do-200/?utm_source=main_tile&utm_medium=smartdo200#zag3'));
